dbd23020f8c270ee51cbe95ccfbbd1da30b9935f
KacperSzt <dsw50737@student.dsw.edu.pl>
2023-06-25 16:06:02 +0200

    rozszerzenie o --logs

--- skrypt.sh
+++ skrypt.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 if [ $1 == "--date" ]; then
 date
+elif [ $1 == "--logs" ]; then
+	x=1
+	while [ $x -le 100 ]
+	do
+    	filename="log${x}.txt"
+    	touch "$filename"
+    	echo "Utworzono plik $filename"
+    	x=$((x+1))
+	done
 else
-echo "podaj prawidłowy argument"
-fi
+	echo "podaj wlasciwy argument"
 
+fi
