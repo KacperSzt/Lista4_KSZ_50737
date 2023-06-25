69e6da35ee5d2e6caa55cd4b391f6bd392309666
KacperSzt <dsw50737@student.dsw.edu.pl>
2023-06-25 16:21:45 +0200

    dodanie argumentu log30

--- skrypt.sh
+++ skrypt.sh
@@ -6,10 +6,24 @@ elif [ $1 == "--logs" ]; then
 	while [ $x -le 100 ]
 	do
     	filename="log${x}.txt"
-    	touch "$filename"
+    	echo "nazwa: $filename" > "$filename"
+	echo "nazwa skryptu: skrypt.sh" >> "$filename"
+	echo "data: $(date)" >> "$filename"
     	echo "Utworzono plik $filename"
     	x=$((x+1))
 	done
+elif [ $1 == "--logs30" ]; then
+        x=1
+        while [ $x -le 30 ]
+        do
+        filename="log${x}.txt"
+        echo "nazwa: $filename" > "$filename"
+	echo "nazwa skryptu: skrypt.sh" >> "$filename"
+	echo "data: $(date)" >> "$filename"
+        echo "Utworzono plik $filename"
+        x=$((x+1))
+        done
+
 else
 	echo "podaj wlasciwy argument"
 
